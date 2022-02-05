bcb6b5f590e43c2e134895229c70062453ed7440
SomaHam44 <77434852+SomaHam44@users.noreply.github.com>
2022-02-05 14:37:23 +0100

    MainController alap

--- src/main/java/hu/petrik/etlap/etlap/MainController.java
+++ src/main/java/hu/petrik/etlap/etlap/MainController.java
@@ -3,14 +3,39 @@ package hu.petrik.etlap.etlap;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextArea;
+
+import java.sql.SQLException;
+import java.util.List;
 
 public class MainController {
-    @FXML
-    private Label welcomeText;
 
+    public TableView<Etlap> etlapTable;
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+    private TableColumn<Etlap, Integer> etlapAr;
+    @FXML
+    private TableColumn<Etlap, String> etlapKategoria;
+    @FXML
+    private TableColumn<Etlap, String> etlapNev;
+    @FXML
+    private TextArea etlapLeiras;
+
+    private EtlapDb db;
+
+
+    private void etlapListaFeltoltes() {
+        try {
+            List<Etlap> etlapList = db.getEtlap();
+            etlapTable.getItems().clear();
+            for (Etlap etlap: etlapList) {
+                etlapTable.getItems().add(etlap);
+            }
+
+        } catch (SQLException e) {
+            System.out.println(e);
+        }
     }
 
     public void onHozzaadButton(ActionEvent actionEvent) {
