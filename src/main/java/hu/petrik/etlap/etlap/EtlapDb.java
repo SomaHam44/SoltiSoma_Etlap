9dba18ab7d7d3bed16416d4daf99d5dfd06113b2
SomaHam44 <77434852+SomaHam44@users.noreply.github.com>
2022-02-05 14:21:14 +0100

    EtlapDb elkezdése, getEtlap metódus

--- src/main/java/hu/petrik/etlap/etlap/EtlapDb.java
+++ src/main/java/hu/petrik/etlap/etlap/EtlapDb.java
@@ -1,7 +1,34 @@
 package hu.petrik.etlap.etlap;
 
-import java.sql.Connection;
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
 
 public class EtlapDb {
     Connection connection;
+
+    public EtlapDb() throws SQLException {
+        this.connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/etlapdb", "root", "");
+
+    }
+
+    public List<Etlap> getEtlap() throws SQLException {
+        List<Etlap> etlapLista = new ArrayList<>();
+        Statement stmt = connection.createStatement();
+        String sql = "SELECT * FROM etlap ";
+        ResultSet resultSet = stmt.executeQuery(sql);
+        while (resultSet.next()) {
+            int id = resultSet.getInt("id");
+            String nev = resultSet.getString("nev");
+            String leiras = resultSet.getString("leiras");
+            int ar = resultSet.getInt("ar");
+            String kategoria = resultSet.getString("kategoria");
+            Etlap etlap = new Etlap(id, nev, leiras, ar, kategoria);
+            etlapLista.add(etlap);
+
+        }
+
+        return etlapLista;
+
+    }
 }
